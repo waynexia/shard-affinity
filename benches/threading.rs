4dcb5e57cc78438abadd073fff16d6212141fed3
Ruihang Xia <waynestxia@gmail.com>
2021-03-18 22:38:02 +0800

    remove unused bench
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- benches/threading.rs
+++
@@ -1,7 +0,0 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use load::ThreadingLoad;
-
-fn bench_threading(c: &mut Criterion) {}
-
-criterion_group!(benches, bench_threading);
-criterion_main!(benches);
